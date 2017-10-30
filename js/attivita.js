80277e2cb87d9de7852ac2bb8df29c50bf52e8bd
edo1998 <edo.iscrizioni@rinonline.com>
2017-10-30 19:17:19 +0100

    Aggiunto Datepicker

--- js/attivita.js
+++ js/attivita.js
@@ -1,9 +1,23 @@
 $( document ).ready(function() {
 	
+	$.fn.datepicker.dates['it'] = {
+    days: ["Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica"],
+    daysShort: ["Lun", "Mar", "Mer", "Gio", "Ven", "Sab", "Dom"],
+    daysMin: ["Lu", "Ma", "Me", "Gi", "Ve", "Sa", "Do"],
+    months: ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
+    monthsShort: ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"],
+    today: "Oggi",
+    clear: "Svuota",
+    format: "dd/mm/yyyy",
+    titleFormat: "MM yyyy", /* Leverages same syntax as 'format' */
+    weekStart: 0
+};
+	
 	$('#data').datepicker({
 	
-		format: 'mm/dd/yyyy',
-		autoclose: true
+		autoclose: true,
+		todayHighlight: true,
+		language: 'it',
 	
 	});
 
