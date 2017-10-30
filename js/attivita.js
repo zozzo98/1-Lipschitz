7621941f3f4dfd5b2706003da1e06fec2177f8e4
edo1998 <edo.iscrizioni@rinonline.com>
2017-10-30 21:48:11 +0100

    Aggiunto Post

--- js/attivita.js
+++ js/attivita.js
@@ -1,17 +1,19 @@
 $( document ).ready(function() {
 	
 	$.fn.datepicker.dates['it'] = {
-    days: ["Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica"],
-    daysShort: ["Lun", "Mar", "Mer", "Gio", "Ven", "Sab", "Dom"],
-    daysMin: ["Lu", "Ma", "Me", "Gi", "Ve", "Sa", "Do"],
-    months: ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
-    monthsShort: ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"],
-    today: "Oggi",
-    clear: "Svuota",
-    format: "dd/mm/yyyy",
-    titleFormat: "MM yyyy", /* Leverages same syntax as 'format' */
-    weekStart: 0
-};
+    
+	    days: ["Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica"],
+	    daysShort: ["Lun", "Mar", "Mer", "Gio", "Ven", "Sab", "Dom"],
+	    daysMin: ["Lu", "Ma", "Me", "Gi", "Ve", "Sa", "Do"],
+	    months: ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
+	    monthsShort: ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"],
+	    today: "Oggi",
+	    clear: "Svuota",
+	    format: "dd/mm/yyyy",
+	    titleFormat: "MM yyyy", /* Leverages same syntax as 'format' */
+	    weekStart: 0
+    
+	};
 	
 	$('#data').datepicker({
 	
@@ -19,6 +21,28 @@ $( document ).ready(function() {
 		todayHighlight: true,
 		language: 'it',
 	
+	});
+	
+	$.validate({
+  	
+  		modules : 'date'
+	
+	});
+	
+	$( "#submit" ).click(function() {
+  	
+  		$.post( "test.php", { 
+	
+			data: $("#data").val(), 
+			tipologia: $("#tipologia").val(), 
+			importo: $("#importo").val(),
+			descrizione: $("#descrizione").val(), 
+			ripetitiva: $("#ripetitiva").val(), 
+			ogni_quanto: $("#ogni_quanto").val(), 
+			a_partire: $("#a_partire").val() 
+		
+		});
+  	
 	});
 
 });
